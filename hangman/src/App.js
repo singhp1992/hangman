1c8c491a8361ed33558e2d5f9ac8ee8c054961e8
Preeti <singhp1992@gmail.com>
2018-04-08 16:07:52 +0200

    update

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import Projects from './components/projects'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hangman </h1>
+        <Projects />
       </div>
     );
   }
---
+++ hangman/src/components/projects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        <h1>My projects </h1>
+      </div>
+    );
+  }
+}
+
+export default Projects;
