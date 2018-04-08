628353f32d4ee79c32979958d2078cc338ba5e6b
Preeti <singhp1992@gmail.com>
2018-04-08 19:15:46 +0200

    attempted letter slots

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hangman </h1>
+        <letterSlots />
         <Projects />
       </div>
     );
--- hangman/src/components/projects.js
+++ hangman/src/components/projects.js
@@ -8,6 +8,33 @@ class Projects extends Component {
         <h5> instructions:</h5>
         <h5> start with one letter and try to guess the correct word :) </h5>
           <h5> goal: don't let the man hang!</h5>
+          <button onClick={ this.toggleEdit }>a</button>
+          <button onClick={ this.toggleEdit }>b</button>
+          <button onClick={ this.toggleEdit }>c</button>
+          <button onClick={ this.toggleEdit }>d</button>
+          <button onClick={ this.toggleEdit }>e</button>
+          <button onClick={ this.toggleEdit }>f</button>
+          <button onClick={ this.toggleEdit }>g</button>
+          <button onClick={ this.toggleEdit }>h</button>
+          <button onClick={ this.toggleEdit }>i</button>
+          <button onClick={ this.toggleEdit }>j</button><br/>
+          <button onClick={ this.toggleEdit }>k</button>
+          <button onClick={ this.toggleEdit }>l</button>
+          <button onClick={ this.toggleEdit }>m</button>
+          <button onClick={ this.toggleEdit }>n</button>
+          <button onClick={ this.toggleEdit }>o</button>
+          <button onClick={ this.toggleEdit }>p</button>
+          <button onClick={ this.toggleEdit }>q</button>
+          <button onClick={ this.toggleEdit }>r</button>
+          <button onClick={ this.toggleEdit }>s</button>
+          <button onClick={ this.toggleEdit }>t</button><br/>
+          <button onClick={ this.toggleEdit }>u</button>
+          <button onClick={ this.toggleEdit }>v</button>
+          <button onClick={ this.toggleEdit }>w</button>
+          <button onClick={ this.toggleEdit }>x</button>
+          <button onClick={ this.toggleEdit }>y</button>
+          <button onClick={ this.toggleEdit }>z</button><br/>
+          <button onClick={ this.toggleEdit }>new game</button>
       </div>
     );
   }
