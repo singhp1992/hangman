ff4ed55f55a5374ce77ad59ce29b84b5f75ec2d1
Preeti <singhp1992@gmail.com>
2018-04-08 16:57:41 +0200

    added picture

--- hangman/src/components/projects.js
+++ hangman/src/components/projects.js
@@ -5,7 +5,9 @@ class Projects extends Component {
   render() {
     return (
       <div className="Projects">
-        <h1>My projects </h1>
+        <h5> instructions:</h5>
+        <h5> start with one letter and try to guess the correct word :) </h5>
+          <h5> goal: don't let the man hang!</h5>
       </div>
     );
   }
