ac6c09c3698b01f802fffcb69fd617b74e5c4ffc
Preeti <singhp1992@gmail.com>
2018-04-09 15:55:36 +0200

    added pictures that wont show up

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import Projects from './components/projects'
 import Alphabet from './components/alphabetButton'
 import './App.css';
-import logo from './stick figure.jpeg'
+import logo from './stickFigure.jpeg'
 import store from './store'
+import picture from './hangman.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <logo />
         <h1>Hangman</h1>
         <Projects />
         <Alphabet />
         <store />
+        <picture />
       </div>
     );
   }
